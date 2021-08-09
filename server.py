d364f1bcd660a7648c3aa38eeebe4d54984db789
Adin <Adinandrewjura@gmail.com>
2021-08-09 16:15:49 -0700

    Update server.py
    port should actually close now when the program is closed

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
 
+serverRunning = True
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
@@ -27,15 +28,17 @@ def handle_client(conn, addr):
 
             print(f"[{addr}] {msg}")
             conn.send("Msg received".encode(FORMAT))
-
+    global serverRunning
+    serverRunning = False
     conn.close()
+    
     
     
 def start():
     server.listen()
     print(f"[LISTENING] Server is listening on {SERVER}")
 
-    while True:
+    while serverRunning:
         conn, addr = server.accept()
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
