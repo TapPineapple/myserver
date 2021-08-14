90cab4435e09308ffe384f76ff53ee36a261efde
Adin <Adinandrewjura@gmail.com>
2021-08-14 16:55:00 -0700

    Update server.py
    The client can now download files from the server by sending an operation of "1".

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket 
 import threading
 import struct
+import os
 
 PORT = 6969
 SERVER = socket.gethostbyname(socket.gethostname())
@@ -22,7 +23,26 @@ def handle_client(conn, addr):
         operation = conn.recv(4) #recieves 4 byte UINT
         operation = struct.unpack('<I', operation)[0]
         #forgive me for what i'm about to write (why does python not have a switch statement? for multithreaded stuff like this dictionarys with methods seems painful)
-        if (operation == 2):
+        if (operation == 1): #send file to client
+            f = open('test.dll', 'rb') #the name of the file to send in current directory
+            fileSize = os.fstat(f.fileno()).st_size
+            conn.send(struct.pack('<I', fileSize)) #turns filesize into an Int so c/c++ can read it normally
+
+            chunkCount = fileSize // 2000
+            remainingBytes = fileSize % 2000
+
+            i = 0
+            while i < chunkCount:
+                conn.send(f.read(2000))
+                i+=1
+
+            if remainingBytes > 0:
+                #send rest of data
+                conn.send(f.read(remainingBytes))
+
+            f.close()
+            
+        elif (operation == 2): #recieve string message from client
             msg_length = conn.recv(4)
             msg_length = struct.unpack('<i', msg_length)[0]
 
@@ -30,7 +50,8 @@ def handle_client(conn, addr):
             print(f"[{addr}] {msg}")
             conn.send("Msg received".encode(FORMAT))
             
-
+        elif (operation == 3): #end connection operation from client
+            connected = False
 
     conn.close()
     
